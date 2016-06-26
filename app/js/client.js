c76e31524271f7355f847d2fec17c84bb258f7d2
Samuil Gospodinov <samuil4@gmail.com>
2016-06-26 21:05:43 +0300

    added devicemotion detection

--- app/js/client.js
+++ app/js/client.js
@@ -1,30 +1,34 @@
 (function () {
 
-    var status = document.getElementById('status');
-    var connection = new WebSocket('ws://127.0.0.1:1337');
+    if (gyro.hasFeature('devicemotion')) {
 
-    if (!localStorage._id) {
-        localStorage._id = Date.now();
+        var status = document.getElementById('status');
+        var connection = new WebSocket('ws://127.0.0.1:1337');
+
+        if (!localStorage._id) {
+            localStorage._id = Date.now();
+        }
+
+        window.WebSocket = window.WebSocket || window.MozWebSocket;
+
+        if (!window.WebSocket) {
+            console.log('WebSocket not supported');
+            return;
+        }
+
+        connection.onopen = function () {
+            status.innerHTML = 'You are in control';
+        };
+
+        connection.onerror = function (error) {
+            console.log('Connection error! Server down?');
+        };
+
+        gyro.startTracking(function(coords) {
+            coords._id = localStorage._id;
+            connection.send(JSON.stringify(coords));
+        });
     }
 
-    window.WebSocket = window.WebSocket || window.MozWebSocket;
-
-    if (!window.WebSocket) {
-        console.log('WebSocket not supported');
-        return;
-    }
-
-    connection.onopen = function () {
-        status.innerHTML = 'You are in control';
-    };
-
-    connection.onerror = function (error) {
-        console.log('Connection error! Server down?');
-    };
-
-    gyro.startTracking(function(coords) {
-        coords._id = localStorage._id;
-        connection.send(JSON.stringify(coords));
-    });
 })();
 
