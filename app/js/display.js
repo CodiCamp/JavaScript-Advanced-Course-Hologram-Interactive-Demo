26610c0db63fdf95dc05b18952e571e70dc9416e
Samuil Gospodinov <samuil4@gmail.com>
2016-06-27 13:36:30 +0300

    updates on display and client

--- app/js/display.js
+++ app/js/display.js
@@ -1,8 +1,14 @@
 (function () {
 
     var box = document.getElementById('box');
-    var connection = new WebSocket('ws://192.168.0.113:1337');
-    var template = '<div class="fish_wrap"><div class="fred"><div class="top_fin"></div><div class="tail_fin"></div> <div class="fish_body"><div class="eye"></div><div class="scale_1"></div><div class="scale_2"></div><div class="scale_3"></div><div class="scale_4"></div></div></div></div>';
+    var connection = new WebSocket('ws://192.168.0.123:1337');
+    var fishTemplate = '<div class="fred"><div class="top_fin"></div><div class="tail_fin"></div> <div class="fish_body"><div class="eye"></div><div class="scale_1"></div><div class="scale_2"></div><div class="scale_3"></div><div class="scale_4"></div></div></div>';
+    var clients = {};
+    var surfacePlaceholders = [];
+
+    for(var i = 1; i<5; i++){
+        surfacePlaceholders.push(document.getElementById('placeholder-' + i));
+    }
 
     window.WebSocket = window.WebSocket || window.MozWebSocket;
 
@@ -17,9 +23,54 @@
 
     connection.onmessage = function (str) {
         try {
-            var json = JSON.parse(str.data);
+            var data = JSON.parse(str.data);
+            var message  = JSON.parse(data.data.text);
+            console.log(message);
+
+            if(!clients[message._id]) {
+
+                spawnFish(message);
+
+            }else {
+
+                updateFishPosition(message);
+            }
+
         } catch (e) {
             console.log('JSON not valid', str.data);
         }
     };
+
+    /**
+     * Creates HTML Fish instance
+     * @param fishData
+     */
+    function spawnFish(fishData) {
+
+        clients[message._id] = message;
+        clients[message._id].fishObjects = [];
+
+        surfacePlaceholders.forEach(function (surface, index) {
+            var fishWrapper = document.createElement('div');
+            fishWrapper.classList.add('fish_wrap');
+            fishWrapper.classList.add('fishData._id');
+            fishWrapper.innerHTML = fishTemplate;
+
+            clients[message._id].fishObjects.push(fishWrapper);
+
+            surface.appendChild(fishWrapper);
+        });
+    }
+
+    function updateFishPosition (fishData) {
+
+        clients[message._id].fishObjects.forEach(function (fishObject){
+            //TO DO: transformation
+            //transform: translate3d(x,y,z);
+        });
+
+
+        clients[message._id] = message;
+    }
+
 })();
